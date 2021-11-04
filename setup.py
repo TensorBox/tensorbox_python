51db9fe60c1a0d231ef22040cdf6cb7e7c599d0a
Batyrlan Nurbekov <batyrlan94@gmail.com>
2021-11-04 16:05:17 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="tensorboxsdk",
-    version="1.0.1",
+    version="1.0.2",
     author="TensorBox",
     author_email="gettensorbox@gmail.com",
     description="Pytnon bindings for the TensorBox API",
